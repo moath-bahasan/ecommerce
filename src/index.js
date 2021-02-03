005d2e71d1ba5c7138c0e44d619cd350dbd32a44
moath bahasan <moazbahasan@gmail.com>
2021-02-03 15:08:08 +0300

    carousel feature

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
+import { ready } from 'jquery';
 
 $(function() {
 
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.add-to-cart-btn').click(function() {
+        alert('أضيف المنتج إلى عربة الشراء');
+    });
+
+    $('#copyright').text("جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
 
 });
