278016c9cf6178c91a581f4ead39b008ca2174ea
moath bahasan <moazbahasan@gmail.com>
2021-02-10 11:23:44 +0300

    carousel feature

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
-import { ready } from 'jquery';
 
 $(function() {
 
@@ -15,5 +14,117 @@ $(function() {
     });
 
     $('#copyright').text("جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
+
+    $('.product-option input[type="radio"]').change(function(){
+        $(this).parents('.product-option').siblings().removeClass('active');
+        $(this).parents('.product-option').addClass('active');
+    });
+
+    //عندما تتغير كمية المنتج
+    $('[data-product-quantity]').on('change', function() {
+
+        //اجلب الكمية الجديدة
+        var newQuantity= $(this).val();
+
+        //ابحث عن السطر الذي يحتوي معلومات هذا المنتج
+        var parent = $(this).parents('[data-product-info]');
+
+        //اجلب سعر القطعة الواحدة من معلومات المنتج
+        var pricePerUnit = parent.attr('data-product-price');
+
+        //السعر الإجمالي للمنتج هو سعر القطعة مضروبا بعددها
+        var totalPriceForProduct = newQuantity * pricePerUnit;
+
+        //عين السعر الجديد ضمن خلية السعر الإجمالي للمنتج في هذاالسطر
+        parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+
+        //حدث السعر الإجمالي لكل المنتجات
+        calculateTotalPrice();
+    });
+
+    $('[data-remove-from-cart]').click(function() {
+        $(this).parents('[data-product-info]').remove();
+        //أعد حساب السعر الإجمالي بعد حذف أحد المنتجات
+        calculateTotalPrice();
+    });
+
+    function calculateTotalPrice() {
+        //أنشئ متغيرا جديدا لحفظ السعر الإجمالي
+        var totalPriceForAllProducts= 0;
+
+        //لكل سطر يمثل معلومات المنتج في الصفحة
+        $('[data-product-info]').each(function() {
+
+            //اجلب سعر القطعة الواحدة من خاصية الموافقة
+            var pricePerUnit = $(this).attr('data-product-price');
+
+            //اجلب كمية المنتج من حقل اختيار الكمية
+            var quantity = $(this).find('[data-product-quantity]').val();
+
+            var totalPriceForProduct = pricePerUnit * quantity;
+
+            //أضف السعر الإجمالي لخذا المنتج إلى السعر الإجمالي لكل المنتجات, واحفظ القيمة في المتغر نفسه
+            totalPriceForAllProducts = totalPriceForAllProducts + totalPriceForProduct;
+        });
+
+        //حدث السعر الإجمالي لكل المنتجات في الصفحة
+        $('#total-price-for-all-products').text(totalPriceForAllProducts + '$');
+    }
+
+    var citiesBycountry = {
+        sa:['جدة','الرياض'],
+        eg:['القاهرة','الاسكندرية'],
+        jo:['عمان','الزرقاء'],
+        sy:['دمشق','حلب','حماه']
+    };
+
+    //عندما يتعير البلد
+    $('#form-checkout select[name="country"]').change(function() {
+
+        //اجلب رمز البلد
+        var country = $(this).val();
+
+        //اجلب مدن هذا البلد من المصفوفة
+        var cities = citiesBycountry[country];
+
+        //فرغ قائمة المدن
+        $('#form-checkout select[name="city"]').empty();
+
+        //إضافة خيار اختر مدينة
+        $('#form-checkout select[name="city"]').append(
+            '<option disabled selected value="">اختر المدينة</option>'
+        );
+
+        //أضف المدن إلى قائمة المدن
+        cities.forEach(function(city) {
+            var newOption = $('<option></option>');
+            newOption.text(city);
+            newOption.val(city);
+            $('#form-checkout select[name="city"]').append(newOption);
+        });
+
+    });
+
+    //عندما تتغير طريقة الدفع
+    $('#form-checkout input[name="payment_method"]').change(function() {
+
+        //اجلب القيمة المختارة حاليا
+        var paymentMethod = $(this).val();
+
+        if (paymentMethod === 'on_delivery') {
+
+            //إذا كانت عند الاستلام, فعطل حقول بطاقة الائتمان
+            $('#credit-card-info input').prop('disabled', true);
+
+        } else {
+            
+            //وإلافعلها
+            $('#credit-card-info input').prop('disabled', false);
+        
+        }
+
+        //بدل معلومات بطاقة الائتمان بين الظهور والإختفاء
+        $('#credit-card-info').toggle();
+    });
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -95,6 +95,21 @@ module.exports = {
             template: "./src/index.html",
         }),
 
+        new HtmlWebpackPlugin({
+            filename: "product.html",
+            template: "./src/product.html",
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "checkout.html",
+            template: "./src/checkout.html",
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "payment.html",
+            template: "./src/payment.html",
+        }),
+
         new MiniCssExtractPlugin({filename: "css/style.css"}),
 
         new OptimizeCSSAssetsPlugin({}),
