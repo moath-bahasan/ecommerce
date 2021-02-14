25fa645f78acd9805e5ff4e1a49995eab7e8eaff
moath bahasan <moazbahasan@gmail.com>
2021-02-14 15:03:00 +0300

    carousel feature

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch';
 
 $(function() {
 
@@ -127,4 +130,17 @@ $(function() {
         $('#credit-card-info').toggle();
     });
 
+    
+        $( "#price-range" ).slider({
+            range: true,
+            min: 50,
+            max: 1000,
+            step: 50,
+            values: [ 250, 800 ],
+            slide: function(event, ui) {
+                $('#price-min').text(ui.values[0]);
+                $('#price-max').text(ui.values[1]);
+            }
+        });
+         
 });
--- webpack.config.js
+++ webpack.config.js
@@ -110,6 +110,16 @@ module.exports = {
             template: "./src/payment.html",
         }),
 
+        new HtmlWebpackPlugin({
+            filename: "search.html",
+            template: "./src/search.html",
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "contact.html",
+            template: "./src/contact.html",
+        }),
+
         new MiniCssExtractPlugin({filename: "css/style.css"}),
 
         new OptimizeCSSAssetsPlugin({}),
