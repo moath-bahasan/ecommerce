2cec43e4ff9058b18094260dacc3858a69a3ace1
moath bahasan <moazbahasan@gmail.com>
2021-07-24 20:04:39 +0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+//import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './scss/style.scss';
 import './css/style.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
--- webpack.config.js
+++ webpack.config.js
@@ -39,15 +39,11 @@ module.exports = {
             },
 
             {
-                test: /\.css$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader, 
-                        options: {
-                           publicPath: '../',
-                        },  
-                       },
-                    'css-loader'
+                    MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'sass-loader'
                 ]
             },
 
